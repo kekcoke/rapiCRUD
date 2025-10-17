113fc891657e5b939e0e05f88b1998e9d46855db
Kevin <kolioane@gmail.com>
2025-10-17 16:59:40 -0700

    JwtBearerSetup.cs expand setup. + Add to ServiceDefaultsExtensions.cs

--- src/rapidCRUD.ServiceDefaults/Authentication/JwtBearerSetup.cs
+++ src/rapidCRUD.ServiceDefaults/Authentication/JwtBearerSetup.cs
@@ -1,7 +1,41 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace rapidCRUD.ServiceDefaults.Authentication;
 
-public class JwtBearerSetup
+public static class JwtBearerSetup
 {
-    
+    public static IServiceCollection AddJwtBearerSetup(this IServiceCollection services, IConfiguration config)
+    {
+        var jwtOptions = new JwtOptions();
+        
+        config.Bind(nameof(JwtOptions), jwtOptions);
+
+        services.AddSingleton(jwtOptions);
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidAudience = jwtOptions.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+                    ClockSkew = TimeSpan.Zero // removes 5-minute default clock skew
+                };
+            });
+        
+        return services;
+    }
 }
