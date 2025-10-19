ba31605a277df5d7eece39242391f62d83dff238
Kevin <kolioane@gmail.com>
2025-10-19 00:01:01 -0700

    Use Authorize sans policy

--- src/rapidCRUD/Features/Authentication/TestController.cs
+++ src/rapidCRUD/Features/Authentication/TestController.cs
@@ -10,7 +10,7 @@ public class TestController : ControllerBase
 {
     [HttpGet("protected")]
     [ApiExplorerSettings(IgnoreApi = true)]
-    [Authorize(Policy = "RequireAuthentication")]
+    [Authorize]
     public IActionResult GetProtected()
     {
         var userName = User.Identity?.Name ?? "Authenticated User";
