83370df59cd94958e2dcba8e582918d1834723c7
Kevin <kolioane@gmail.com>
2025-10-20 10:56:35 -0700

    FluentAssertions and EFCore.InMemory nugets to test proj. Init ItemRepositoryTests.cs

---
+++ src/rapidCRUD.Tests/Unit/ItemRepositoryTests.cs
@@ -0,0 +1,13 @@
+using rapidCRUD.Features.Items;
+using FluentAssertions;
+using Xunit;
+using rapidCRUD.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace rapidCRUD.Tests.Unit;
+
+[Trait("Category", "Unit")]
+public class ItemRepositoryTests
+{
+    
+}
