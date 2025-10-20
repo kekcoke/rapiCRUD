95d5c695e523f1815b11ef25e705cd5c5c4f172c
Kevin <kolioane@gmail.com>
2025-10-20 11:58:07 -0700

    UpdateAsync_ShouldUpdateItemInDatabase test

--- src/rapidCRUD.Tests/Unit/ItemRepositoryTests.cs
+++ src/rapidCRUD.Tests/Unit/ItemRepositoryTests.cs
@@ -36,4 +36,26 @@ public class ItemRepositoryTests
         result.Should().NotBeNull();
         result!.Name.Should().Be("Test Item");
     }
+
+    [Fact]
+    public async Task UpdateAsync_ShouldUpdateItemInDatabase()
+    {
+        await using var context = GetInMemoryDbContext();
+        var repository = new ItemRepository(context);
+        var item = new Item
+        {
+            Id = Guid.NewGuid(),
+            Name = "Initial Item",
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.Now
+        };
+        await repository.AddAsync(item);
+        
+        item.Name = "Updated Item";
+        await repository.UpdateAsync(item);
+        var result = await repository.GetByIdAsync(item.Id);
+        
+        result.Should().NotBeNull();
+        result!.Name.Should().Be("Updated Item");
+    }
 }
