74a249028ddd545c420c7dfbfbb0ae57acd68cef
Kevin <kolioane@gmail.com>
2025-10-20 11:42:43 -0700

    ItemRepositoryTests: AddAsync_ShouldAddItemToDatabase

--- src/rapidCRUD.Tests/Unit/ItemRepositoryTests.cs
+++ src/rapidCRUD.Tests/Unit/ItemRepositoryTests.cs
@@ -9,5 +9,31 @@ namespace rapidCRUD.Tests.Unit;
 [Trait("Category", "Unit")]
 public class ItemRepositoryTests
 {
-    
+    private ApplicationDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+            options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+        
+        return new ApplicationDbContext(options.Options);
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldAddItemToDatabase()
+    {
+        await using var context = GetInMemoryDbContext();
+        var repository = new ItemRepository(context);
+        var item = new Item
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Item",
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.Now
+        };
+        
+        await repository.AddAsync(item);
+        var result = await repository.GetByIdAsync(item.Id);
+        
+        result.Should().NotBeNull();
+        result!.Name.Should().Be("Test Item");
+    }
 }
