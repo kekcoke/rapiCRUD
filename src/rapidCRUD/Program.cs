0a03c540bb6ba05e1ee4b5adf6b1bbe3fac0ed84
Kevin <kolioane@gmail.com>
2025-10-20 15:17:52 -0700

    Install ApplicationInsights + set on Program.cs

--- src/rapidCRUD/Program.cs
+++ src/rapidCRUD/Program.cs
@@ -194,6 +194,17 @@ app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 
+// Application Insights
+if (!string.IsNullOrEmpty(builder.Configuration["ApplicationInsights:ConnectionString"]))
+{
+    builder.Services.AddApplicationInsightsTelemetry(options =>
+    {
+        options.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"];
+        options.EnableAdaptiveSampling = true;
+        options.EnableQuickPulseMetricStream = true;
+    });
+}
+
 // Health Checks Endpoints
 app.MapHealthChecks("/health");
 app.MapHealthChecks("/health/ready", new HealthCheckOptions()
